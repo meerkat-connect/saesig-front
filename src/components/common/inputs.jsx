554068c6909b859e0326e30bac981206c9b21aed
beiku <pbeiku@gmail.com>
2023-09-01 14:52:08 +0900

    style : 식구등록 퍼블리시 완료

--- src/components/common/inputs.jsx
+++ src/components/common/inputs.jsx
@@ -27,9 +27,13 @@ export const Dropdown = ({ label, options, ...rest }) => {
   };
 
   return (
-    <div className={`${isOpen && 'ss-input__dropdown--active'} ss-input__dropdown`} ref={dropdownRef}>
+    <div className={`${isOpen && 'ss-input__label'} ss-input__dropdown`} ref={dropdownRef}>
+      {label ? (
+        <label className="ss-input__label --required mb-12">{label}</label>
+      ) : (
+        <label className="ss-input__label mb-12">&nbsp;</label>
+      )}
       <input type="text" value={selectedOption} readOnly onClick={handleInputClick} {...rest} />
-      {label && <label>{label}</label>}
       {isOpen && options && (
         <div className="ss-input__options">
           {options.map((option, i) => (
@@ -47,7 +51,7 @@ export const PriceInput = ({ label, ...rest }) => {
   const required = rest.required || false;
   return (
     <div className="relative">
-      {label && <label className={`${required && 'ss-input__label--required'} ss-input__label`}>{label}</label>}
+      {label && <label className={`${required && 'ss-input__label --required'} ss-input__label`}>{label}</label>}
       <div className="relative">
         <input className="ss-input mt-12" type="text" {...rest} />
         <span className="ss-input__price">원</span>
@@ -64,7 +68,7 @@ export const TextInput = ({ label, ...rest }) => {
   const required = rest.required || false;
   return (
     <div>
-      {label && <label className={`${required && 'ss-input__label--required'} ss-input__label`}>{label}</label>}
+      {label && <label className={`${required && 'ss-input__label --required'} ss-input__label`}>{label}</label>}
       <input className="ss-input mt-12" type="text" {...rest} />
     </div>
   );
@@ -74,6 +78,7 @@ export const AgeInput = ({ label, options, ...rest }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState('');
   const dropdownRef = useRef(null);
+  const required = rest.required || false;
 
   useEffect(() => {
     const handleClickOutside = (e) => {
@@ -97,26 +102,36 @@ export const AgeInput = ({ label, options, ...rest }) => {
   };
 
   return (
-    <div className={`${isOpen && 'ss-input__dropdown--active'} ss-input__dropdown relative`} ref={dropdownRef}>
-      <input
-        className="ss-input__dropdown--age"
-        type="text"
-        value={selectedOption}
-        readOnly
-        onClick={handleInputClick}
-        {...rest}
-      />
-      <input type="text" />
-      {label && <label>{label}</label>}
-      {isOpen && options && (
-        <div className="ss-input__options">
-          {options.map((option, i) => (
-            <div key={i} onClick={() => handleOptionClick(option)}>
-              {option}
-            </div>
-          ))}
+    <div>
+      {label && <label className={`${required && 'ss-input__label --required'} ss-input__label`}>{label}</label>}
+      <div className="ss-input__with-dropdown mt-12" ref={dropdownRef}>
+        <input className="ss-input" type="text" required />
+        <div className={`${isOpen && '--open'}`}>
+          <input type="text" value={selectedOption} readOnly onClick={handleInputClick} {...rest} />
+          <div />
         </div>
-      )}
+        {isOpen && options && (
+          <ul>
+            {options.map((option, i) => (
+              <li key={i} onClick={() => handleOptionClick(option)}>
+                {option}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export const TextArea = ({ label, ...rest }) => {
+  const required = rest.required || false;
+
+  return (
+    <div className="ss-input__textarea">
+      {label && <label className={`${required && 'ss-input__label --required'} ss-input__label`}>{label}</label>}
+      <textarea className="mt-12" cols="30" rows="10" placeholder="내용을 입력해주세요" required></textarea>
+      <div className="pt-10">0/1,000자</div>
     </div>
   );
 };
