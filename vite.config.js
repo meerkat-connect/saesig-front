3c50d85ea174c0f04bfebc40f61cc910d4631595
jedje <jedje95@gmail.com>
2023-10-18 02:32:11 +0900

    chore: API 호출을 위한 axois 추가

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  // 개발 서버 설정
+  server: {
+    // 프록시 설정
+    proxy: {
+      // api 로 시작되는 api 는 아래 주소로 프록시 설정해서 쏴라
+      '/api': {
+        // 프록시 요청을 보낼 서버의 주소
+        // TODO 로컬/배포 환경 실행환경 분리
+        target: 'http://localhost:8080'
+      }
+    }
+  }
 });
