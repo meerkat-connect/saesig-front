42f5ac2d96f0270758f5b2bf071d4e9e9872d1b6
jedje <jedje95@gmail.com>
2023-10-18 16:21:58 +0900

    chore: 개발/운영 실행환경 분리
    - npm run build: .env.production 파일의 환경변수 세팅
    - npm run dev: .env.development 파일의 환경변수 세팅

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command, mode }) => {
+  // load .env vars
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  // 개발 서버 설정
-  server: {
-    // 프록시 설정
-    proxy: {
-      // api 로 시작되는 api 는 아래 주소로 프록시 설정해서 쏴라
-      '/api': {
-        // 프록시 요청을 보낼 서버의 주소
-        // TODO 로컬/배포 환경 실행환경 분리
-        target: 'http://localhost:8080'
+    // 개발 서버 설정
+    server: {
+      // 프록시 설정
+      proxy: {
+        // api 로 시작되는 api 는 아래 주소로 프록시 설정
+        '/api': {
+          // 프록시 요청을 보낼 서버의 주소
+          // .env 파일에 정의되어 있으며 build 시 production 폴더의 변수를 바라봄
+          target: env.VITE_APP_API_BASE_URL
+        }
       }
     }
   }
