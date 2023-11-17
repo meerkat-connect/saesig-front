a71f626b5b22ea7ca3ce232d6359a0eed22390e9
beiku <pbeiku@gmail.com>
2023-11-17 19:25:43 +0900

    style : 일상기록 Index 페이지 퍼블리싱 완료

---
+++ src/pages/diary/_components/gallery.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MasonryGrid } from '@egjs/react-grid';
+
+export default function Gallery({ galleryData }) {
+  return (
+    <MasonryGrid
+      className="main__gallery"
+      gap={20}
+      defaultDirection={'end'}
+      align={'stretch'}
+      column={2}
+      columnSize={0}
+      columnSizeRatio={0}
+    >
+      {galleryData?.map((gallery) => (
+        <div className={'gallery__item'} key={gallery.id}>
+          <img src={gallery.image} alt="" loading="lazy" />
+          <h6 className="ml-20">{gallery.title}</h6>
+          <div>
+            <span>{gallery.date}</span>
+            <span>by {gallery.author}</span>
+          </div>
+        </div>
+      ))}
+    </MasonryGrid>
+  );
+}
