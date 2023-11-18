a3afe94f3a0f616235be82cea034745552c9adfe
이동헌 <ann959896@gmail.com>
2023-11-19 02:31:22 +0900

    chore: 프론트 서버 구동용 파일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 // pages
 import Layout from './components/layout/layout';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Login from './pages/accounts/login';
 import SignUp from './pages/accounts/join';
 import NotFound from './pages/error/404';
