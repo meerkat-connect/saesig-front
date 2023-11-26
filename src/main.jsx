32da66583a7ea62bfdf8bdea17abccbe44d4ea0c
서정도 <sjd4328@kbrainc.com>
2023-11-27 01:15:47 +0900

    ongoing : Google Analytics 중간저장

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './scss/main.scss';
 import ReactGA from "react-ga";
 
 // Google tag (gtag.js)
-const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID;
+const gaTrackingId = import.meta.env.VITE_APP_GA_TRACKING_ID;
 console.log(gaTrackingId)
 ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
 ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정;
